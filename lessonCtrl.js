f83bc1329b1fcd239f50e4b864dd9fb2b6a376a0
Thad Paget <fubekafilms@gmail.com>
2017-02-28 20:13:07 -0700

    linked custom directive

--- lessonCtrl.js
+++ lessonCtrl.js
@@ -1,7 +1,7 @@
 angular.module('directivePractice').controller('lessonCtrl', function($scope){
 
   $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
-  $scope.dayAlert = function announceDay(lesson, day) {
-    return lesson + ' is active on ' + day + '.';
+  $scope.announceDay = function announceDay(lesson, day) {
+    alert(lesson + ' is active on ' + day + '.');
   }
 })
